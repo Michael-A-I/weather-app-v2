127a2df822093389dcf361a81ce6242bf38796f4
michael aduayi <zero.aduayi@gmail.com>
2021-03-17 00:14:37 -0400

    city json added

--- public/index.js
+++ public/index.js
@@ -10,39 +10,38 @@
 .##.....##.##........####
 */
 
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c144deeef83c9fc568b52b95903fb474', true)
-
-request.onload = function () {
-  // Begin accessing JSON data here
-  let data = JSON.parse(this.response)
-  console.log(data);
-}
-
-// Send request
-// request.send()
-
-
-
-function search(response){
-    
-}
-
-
 const apiKey = "c144deeef83c9fc568b52b95903fb474";
 
 const apiCall = "api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}"
 
 const endpoint = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=c144deeef83c9fc568b52b95903fb474"
 // const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+// const citiesFile = require('../city.list.json');
+// var mydata = JSON.parse(city.list.json);
+// console.log(mydata);
 const cities = []
+
+
+
+
+// fetch list of cities to regex through and enter into API call. 
+fetch("./city.list.json")
+.then(response => {
+  if(!response.ok){
+    throw new Error("http error"+response.status)
+  }
+  return response.json();
+})
+.then(response => console.log(response))
+// TODO: hook regex function to above data 1. display potential list of data 2.apon selection enter into api and do a call.
+
 
 fetch(endpoint)
 .then(data => data.json())
 .then(data => cities.push(data))
+//TODO: fetch weather data for selected city with this dynamically 1. display data selected. 
+
 
 // Reg
 function findMatches(wordToMatch,cities){
@@ -52,3 +51,16 @@ function findMatches(wordToMatch,cities){
     return place.name.match(regex) || place.sys.country.match(regex)
   })
 }
+
+
+
+
+
+//look through list of cities in the city.list.json file
+
+// once user selects city in gui
+
+// api call for data regarding city selected
+
+// display data onto webpage. 
+
