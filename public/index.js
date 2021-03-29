591baff073cd0550774fc0dc08c43c0b3e783253
Michael Aduayi <zero.aduayi@gmail.com>
2021-03-29 16:26:08 -0400

    latest 3.29

--- public/index.js
+++ public/index.js
@@ -20,6 +20,9 @@ const cities = []
 
 let input = document.querySelector("#w-location")
 
+
+// const cityListItem = document.querySelector(".city-list-item")
+
 // fetch list of cities to regex through and enter into API call. 
 fetch("./city.list.json")
   .then(response => {
@@ -43,21 +46,33 @@ fetch(endpoint)
 // RegExp
 function findMatches(wordToMatch, cities) {
 
+  // array of citiy objects found via regex stored in array
+  let matchesFound = []
+
   // console.log(cities)
   return cities.filter(function (places, index) {
     //     figure out if city or stat matches typed search
     const regex = new RegExp(wordToMatch, 'gi')
+    const cityUl = document.querySelector(".city-list")
 
-    places.forEach(place => {
-      if(place.name.match(regex)){
-        
-        console.log(place); 
-
+    places.forEach(function(place, index) {
+      if(place === ""){
+        return;
       }
 
-    
+      if(place.name.match(regex) || place.country.match(regex)){
+        // push cities obj. data into local context.
+        matchesFound.push(place)
+      }
+      
+      
     });
-
+    
+    console.log(matchesFound);
+    // display list of elements in dom
+    displayList(cityUl,matchesFound);
+   
+    
 
   })
 
@@ -70,9 +85,31 @@ function inputMatches() {
   // console.log(input.value)
   // console.log(cities)
   findMatches(input.value, cities)
+  // console.log(place)
+
 }
 
 
+
+function displayList(domElement,matchesFound){
+  //take in a dom element which should be global anyways. 
+  // take in data within execution context 
+  // dropdown menu
+  // loop through data and append data to dom list. 
+  matchesFound.forEach(element => {
+    console.log(element.name);
+
+  let li = document.createElement("li");
+  // set id of html element to object id in JSON file
+  li.setAttribute('id',element.id);
+  li.appendChild(document.createTextNode(`${element.name},${element.state || element.country}`));
+  domElement.appendChild(li);
+  });
+
+
+}
+
+  
 
 
 
@@ -82,6 +119,7 @@ function inputMatches() {
 //look through list of cities in the city.list.json file
 
 // once user selects city in gui
+// create function that will take search data and place it in the gui. as a link 4 api call. 
 
 // api call for data regarding city selected
 
