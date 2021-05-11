cb2268dc7e3922a3d8d753259cbb79e6029f2a47
michael aduayi <zero.aduayi@gmail.com>
2021-05-10 20:54:40 -0400

    tool tips added

---
+++ public/js/tooltip.js
@@ -0,0 +1,5 @@
+ // With the above scripts loaded, you can call `tippy()` with a CSS
+      // selector and a `content` prop:
+      tippy('#w-location', {
+        content: 'Enter city location. ex: Boston (will work after 3 characters)',
+      });
